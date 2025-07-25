359d030a6e38c08835ff2119144c052a9bb6e72e
Hieu Truong <truongduchieux6qt@gmail.com>
2025-07-25 10:45:47 +0700

    update collision system and AI

---
+++ Assets/Scirpts/Pickup/BlockSpawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [SerializeField] private int totalBlocks = 50;
+    [SerializeField] private int minEachColor = 8;
+    [SerializeField] private float spawnRange = 10f;
+
+    private void Start()
+    {
+        SpawnRandomBlocks();
+    }
+
+    private void SpawnRandomBlocks()
+    {
+        int colorCount = System.Enum.GetValues(typeof(BlockColor)).Length;
+        int[] colorTotals = new int[colorCount];
+
+        // Đảm bảo mỗi màu có ít nhất minEachColor
+        int placed = 0;
+        for (int i = 0; i < colorCount; i++)
+        {
+            for (int j = 0; j < minEachColor; j++)
+            {
+                SpawnBlock((BlockColor)i);
+                colorTotals[i]++;
+                placed++;
+            }
+        }
+
+        // Phần còn lại spawn ngẫu nhiên
+        while (placed < totalBlocks)
+        {
+            int colorIndex = Random.Range(0, colorCount);
+            SpawnBlock((BlockColor)colorIndex);
+            colorTotals[colorIndex]++;
+            placed++;
+        }
+    }
+
+    private void SpawnBlock(BlockColor color)
+    {
+        GameObject blockObj = BlockPool.Instance.Get(color);
+        blockObj.transform.position = new Vector3(
+            Random.Range(-spawnRange, spawnRange),
+            0.8f,
+            Random.Range(-spawnRange, spawnRange)
+        );
+        blockObj.transform.rotation = Quaternion.identity;
+        blockObj.SetActive(true);
+
+        PickupBlock pickup = blockObj.GetComponent<PickupBlock>();
+        pickup.ResetState();
+
+        BlockManager.Instance.RegisterBlock(pickup);
+    }
+}
---
+++ Assets/Scirpts/Pickup/PickupBlock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PickupBlock : MonoBehaviour
+{
+    [field: SerializeField] public BlockColor blockColor { get; private set; }
+    public bool IsCollected { get; private set; }
+
+    private void OnEnable()
+    {
+        IsCollected = false;
+    }
+
+    public void MarkCollected()
+    {
+        IsCollected = true;
+    }
+
+    public void ResetState()
+    {
+        IsCollected = false;
+        gameObject.SetActive(true);
+    }
+
+    public void ReturnToPool()
+    {
+        IsCollected = false;
+        BlockManager.Instance.UnregisterBlock(this);
+        BlockPool.Instance.Return(gameObject, blockColor);
+    }
+}
